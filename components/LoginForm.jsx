a2d5ec01f05041b040c847c01a180265f3aea2d4
Tanvir <tanvir.info07@gmail.com>
2024-12-25 23:19:07 +0600

    only setup ready

---
+++ components/LoginForm.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const LoginForm = () => {
+  return (
+    <form className="space-y-4 text-gray-600">
+      <input
+        type="email"
+        placeholder="Email"
+        className="w-full border border-gray-300  rounded-full px-4 py-3 focus:outline-none focus:ring-2 focus:ring-primary"
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        className="w-full border border-gray-300 rounded-full px-4 py-3 focus:outline-none focus:ring-2 focus:ring-primary"
+      />
+
+      <button
+        type="submit"
+        className="w-full bg-primary text-white rounded-full py-3 hover:bg-primary transition"
+      >
+        Continue
+      </button>
+    </form>
+  );
+};
+
+export default LoginForm;
